0c501349abf15e156c0203ab4cc05f5465e9d93b
RodolGiaco <rodolfo.giacomodonatto@sitrack.com>
2026-09-04 12:59:37 -0300

    chore: rename package

--- src/main/java/com/acme/customer/api/CustomerApiApplication.java
+++ src/main/java/com/acme/customer/api/CustomerApiApplication.java
@@ -1,4 +1,4 @@
-package com.acme.customer_api;
+package com.acme.customer.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
