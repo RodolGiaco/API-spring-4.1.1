d0aeee794fa4df76f03fd2532e04ea9bbcfb3ebd
RodolGiaco <rodolfo.giacomodonatto@sitrack.com>
2026-09-04 16:08:37 -0300

    feat: add customer request validation

---
+++ src/main/java/com/acme/customer/api/CustomerCreateRequest.java
@@ -0,0 +1,16 @@
+package com.acme.customer.api;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public record CustomerCreateRequest(
+
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Email
+        String email
+
+) {
+}
--- src/test/java/com/acme/customer/CustomerApiApplicationTests.java
+++ src/test/java/com/acme/customer/CustomerApiApplicationTests.java
@@ -1,4 +1,4 @@
-package com.acme.customer.api;
+package com.acme.customer;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
